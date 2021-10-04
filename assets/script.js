15a6bfeda976e312d75576b6162ffa97f9a3ff3c
LATOMMYLA <tommyl.dmd@gmail.com>
2021-10-03 21:38:35 -0700

    added question varible with options and answer

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,28 @@
-
+// Question variables 
+var questions = [
+ {
+  question: "Inside which HTML element do we put the JavaScript?",
+  options: ["<script>", "<scripting>", "<js>", "<javascript>"],
+  answer: "<script>"
+ },
+ {
+  question: "How do you write 'Hello World' in an alert box?",
+  options: ["alert('Hello World');", "alertBox('Hello World');", "msgBox('Hello World');", "msg('Hello World');"],
+  answer: "alert('Hello World')"
+ },
+ {
+  question: "Which event occurs when the user clicks on an HTML element?",
+  options: ["onchange", "onmouseclick", "onclick", "onmouseover"],
+  answer: "onclick"
+ },
+ {
+  question: "How to write an IF statement in JavaScript?",
+  options: ["if i = 5 then", "if(i == 5)", "if i == 5 then", "if i = 5"],
+  answer: "if(i == 5)"
+ },
+ {
+  question: "What is the correct way to write a Javascript array?",
+  options: ["var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')", "var colors = (1:'red', 2:'green', 3:'blue')", "var colors = ['red', 'green', 'blue']", "var colors = 'red', 'green', 'blue'"],
+  answer: "var colors = ['red', 'green', 'blue']"
+ }
+]
