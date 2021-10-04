987d220c821f421d44571c87c44b4c6683640806
LATOMMYLA <tommyl.dmd@gmail.com>
2021-10-04 16:53:38 -0700

    structure developed

---
+++ assets/quiz.js
@@ -0,0 +1,23 @@
+var finalScores = document.querySelector("#finalscores");
+var clear = document.querySelector("#reset");
+var back = document.querySelector("#back");
+
+clear.addEventListener("click", function () {
+ localStorage.clear();
+ location.reload();
+});
+
+var scoreRecap = localStorage.getItem("scoreRecap");
+scoreRecap = JSON.parse(scoreRecap);
+
+if (scoreRecap !== null) {
+ for (var i = 0; i < scoreRecap.length; i++) {
+  var createLi = document.createElement("li");
+  createLi.textContent = scoreRecap[i].initials + " " + scoreRecap[i].score;
+  finalScores.appendChild(createLi);
+ }
+}
+
+back.addEventListener("click", function () {
+ window.location.replace("./index.html");
+});
